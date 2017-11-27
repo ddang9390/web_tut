1b8d42be7e03823c0defe2907b01f8e2d3b33f10
Daniel <dnld1ng@yahoo.com>
2017-11-26 22:31:06 -0500

    Get Post Forms

---
+++ get_target.php
@@ -0,0 +1,13 @@
+<head>
+	<title>GET</title>
+</head>
+
+<body>
+	<p>First Name: <?php echo $_GET["fname"]; ?></p>
+	<p>Last Name: <?php echo $_GET["lname"]; ?></p>
+	<p>Age: <?php echo $_GET["age"]; ?></p>
+	
+	<p>State: <?php echo $_GET["state"]; ?></p>
+	<p>Gender: <?php echo $_GET["gender"]; ?></p>
+	
+</body>
---
+++ post_target.php
@@ -0,0 +1,13 @@
+<head>
+	<title>POST</title>
+</head>
+
+<body>
+	<p>First Name: <?php echo $_POST["fname"]; ?></p>
+	<p>Last Name: <?php echo $_POST["lname"]; ?></p>
+	<p>Age: <?php echo $_POST["age"]; ?></p>
+	
+	<p>State: <?php echo $_POST["state"]; ?></p>
+	<p>Gender: <?php echo $_POST["gender"]; ?></p>
+	
+</body>
